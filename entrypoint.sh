f325ea5fd46c6f475ca3169d3d2c41e4f5a50e40
Kot <kot@yukata.tech>
2022-09-18 20:22:38 -0700

    Document swap command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Allocate 70% of free disk space as swap
+# %a - Free blocks available to non-superuser
+# %s - Block size
+# Taken from https://community.fly.io/t/swap-memory/2749/2?u=kot
 if [[ ! -z "$SWAP" ]]; then fallocate -l $(($(stat -f -c "(%a*%s/10)*7" .))) _swapfile && mkswap _swapfile && swapon _swapfile && ls -hla; fi; free -m
 
 CODER_PG_CONNECTION_URL="${DATABASE_URL}?sslmode=disable" su-exec coder:coder /opt/coder server
