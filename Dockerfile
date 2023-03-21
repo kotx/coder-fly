ac464910076755b9b30c61ac7bb1f8e1f7fad536
Kot <kot@yukata.tech>
2023-03-21 10:54:52 -0700

    Update Coder version & Fly config

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM ghcr.io/coder/coder:latest
 EXPOSE 8080
-ENV CODER_ADDRESS=0.0.0.0:8080
+ENV CODER_HTTP_ADDRESS=0.0.0.0:8080
 
 #USER coder # this is set in entrypoint with su-exec
+#hadolint ignore=DL3002
 USER root
 RUN apk add --no-cache su-exec
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 # %a - Free blocks available to non-superuser
 # %s - Block size
 # Taken from https://community.fly.io/t/swap-memory/2749/2?u=kot
-if [[ ! -z "$SWAP" ]]; then fallocate -l $(($(stat -f -c "(%a*%s/10)*7" .))) _swapfile && mkswap _swapfile && swapon _swapfile && ls -hla; fi; free -m
+if [ -n "$SWAP" ]; then fallocate -l $(($(stat -f -c "(%a*%s/10)*7" .))) _swapfile && mkswap _swapfile && swapon _swapfile && ls -hla; fi; free -m
 
 # Set swappiness to 10
 sysctl -w vm.swappiness=10
 
 mkdir /.cache && chown coder:coder /.cache
 
-CODER_PG_CONNECTION_URL="${DATABASE_URL}?sslmode=disable" su-exec coder:coder /opt/coder server
+CODER_PG_CONNECTION_URL="${DATABASE_URL}" su-exec coder:coder /opt/coder server
